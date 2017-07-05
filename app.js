7ad3bfbebfdc07700e7a96819992cdc1c9e31bea
Yu Jin Seong <abc9816@ajou.ac.kr>
2017-07-05 15:49:21 +0900

    adding my files what i did

---
+++ app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = function (requestNumber) {
+  /*  
+    Node.js와 JavaScript 기초를 위한 문제 입니다.
+    module.exports 등은 지금 단계에서는 무시해도 됩니다.
+    FizzBuzz 퀴즈는 다음과 같습니다.
+    - 3의 배수이면 fizz를 리턴한다.
+    - 5의 배수이면 buzz를 리턴한다.
+    - 15의 배수이면 fizzbuzz를 리턴한다.
+    - 이 밖의 숫자는 해당 숫자를 리턴한다.
+    작성한 코드가 정상적인지 확인하려면 test.js 파일을 실행하세요
+
+    실행 예
+    ```javascript
+    node test.js
+    ```
+    VSCode를 사용하는 경우 Debug 탭에서 테스트를 실행하세요
+    
+    완료되면 Github에 저장소를 만든 후 본 프로젝트를 업로드 한 후 알려주세요
+        
+    아래에 코드를 작성하세요
+  */
+  var temp_three = requestNumber % 3;
+  var temp_five = requestNumber % 5;
+  var temp_both = requestNumber % 15;
+
+  if(temp_both == 0){
+    return 'fizzbuzz';
+  }else{
+    if(temp_three == 0){
+      return 'fizz';
+    }else if(temp_five == 0){
+      return 'buzz';
+    }else{
+      return requestNumber;
+    }
+  }
+}
